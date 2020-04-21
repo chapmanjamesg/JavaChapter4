faa15c82224557579f86d625b98fba79831cdf96
James Chapman <chapmanjamesg@gmail.com>
2020-04-21 07:53:47 -0500

    Finished the first exercise in chapter 4

---
+++ DemoBlock.java
@@ -0,0 +1,27 @@
+public class DemoBlock{
+    public static void main(String[] args) {
+        System.out.println("Demonstrating block scope");
+        int x = 1111;
+        System.out.println("In the first block x= " + x);
+        {
+            int y = 2222;
+            System.out.println("In the second block x= " + x);
+            System.out.println("In the second block y= " + y);
+        }
+        {
+            int y = 3333;
+            System.out.println("In the third block x= " + x);
+            System.out.println("In the third block y= " + y);
+            demoMethod();
+            System.out.println("After method x is " + x);
+            System.out.println("After method block y is " + y);
+        }
+        System.out.println("At the end x is  " + x);
+    }
+    public static void demoMethod() {
+        int x = 8888, y = 9999;
+        System.out.println("In demoMethod x is " + x);
+        System.out.println("In demoMethod block y is " + y);
+        
+    }
+}
